d06f372a5eed8cdabcb903525078cd6a96cdf881
andersonpog <silveira.anderson.1988@gmail.com>
2026-04-05 01:23:44 -0300

    Regex e gerador de token

---
+++ JackTokenizer.py
@@ -0,0 +1,56 @@
+import re
+import sys
+
+# Regex para os tokens da linguagem Jack
+TOKEN_SPEC = [
+    ('KEYWORD',    r'\b(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)\b'),
+    ('SYMBOL',     r'([{}()\[\],.;+\-*/&|<>=~])'),
+    ('INT_CONST',  r'(\d+)'),
+    ('STR_CONST',  r'"([^"\n]*)"'),
+    ('IDENTIFIER', r'([a-zA-Z_][a-zA-Z_0-9]*)'),
+    ('COMMENT',    r'//.*|/\*[\s\S]*?\*/'), # Comentários de linha e bloco
+    ('SPACE',      r'\s+'),                 # Espaços e quebras de linha
+]
+
+# Compila a regex unificada
+MASTER_RE = re.compile('|'.join(f'(?P<{name}>{pattern})' for name, pattern in TOKEN_SPEC))
+
+class JackTokenizer:
+    def __init__(self, filename):
+        try:
+            with open(filename + ".jack", 'r') as f:
+                self.source = f.read()
+        except:
+            print("Arquivo não encontrado. Digite o nome do arquivo jack sem a extensão.")
+            sys.exit(1)
+
+
+        
+        self.tokens = []
+        self._tokenize()
+        self.current_token_idx = -1
+
+    def _tokenize(self):
+        """Transforma o código fonte em uma lista de tuplas (tipo, valor)"""
+        for match in MASTER_RE.finditer(self.source):
+            kind = match.lastgroup
+            value = match.group(kind)
+            
+            if kind == 'COMMENT' or kind == 'SPACE':
+                continue
+            elif kind == 'STR_CONST':
+                # Remove as aspas da string para o valor interno
+                value = value[1:-1]
+            
+            self.tokens.append((kind, value))
+
+    def has_more_tokens(self):
+        return self.current_token_idx + 1 < len(self.tokens)
+
+    def advance(self):
+        if self.has_more_tokens():
+            self.current_token_idx += 1
+            self.current_type, self.current_value = self.tokens[self.current_token_idx]
+
+    def content(self):
+        print(self.tokens)
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sys
 
+from JackTokenizer import JackTokenizer
+
 def main():
     args = sys.argv
 
-    if(args.__len__() > 1):
-        print("Realizando análise léxica do arquivo " + args[1] + ".jack")
-    else:
-        print("Digite o nome do arquivo jack sem a extensão")
+    if(args.__len__() < 2):
+        print("Digite o nome do arquivo jack sem a extensão.")
+        sys.exit(1)
+    
+    print("Realizando análise léxica do arquivo " + args[1] + ".jack")
+
+    tokenizer = JackTokenizer(args[1])
+    tokenizer.content()
+        
+        
 
 if __name__ == "__main__":
     main()
