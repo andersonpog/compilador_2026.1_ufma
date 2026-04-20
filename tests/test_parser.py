cfeb3c69b0571443dfea85bf0f3e15209cd1fedd
Jeys Almone <jeys532667@gmail.com>
2026-04-20 10:58:41 -0300

    Nesta etapa foram adicionados testes para parse_term conforme a estrutura da aula, incluindo o arquivo term_test.jack organizado na pasta tests antes da implementação no parser.py

---
+++ tests/test_parser.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from JackTokenizer import JackTokenizer
+from parser import Parser
+
+def test_parse_term_integer():
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, "term_test")
+
+    tokenizer = JackTokenizer(file_path)
+    tokens = tokenizer.tokens
+
+    parser = Parser(tokens)
+    parser.parse_term()
+    xml = parser.get_xml()
+
+    assert "<term>" in xml
+    assert "<integerConstant> 10 </integerConstant>" in xml
